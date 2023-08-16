e75230ea2f5f69ac8b86ad1b37f14dafddb824cf
Dozhin.V <dozhin.v@gmail.com>
2023-08-16 17:24:44 +0300

    simplify return

--- src/Praktikum.java
+++ src/Praktikum.java
@@ -16,8 +16,6 @@ public class Praktikum {
 
     public static boolean isLeapYear(int year) {
         // здесь нужно определить, является ли переданный год високосным
-        if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0)) {
-            return true;
-        } else return false;
+        return (year % 4 == 0) && (year % 400 == 0 || year % 100 != 0);
     }
-} 
+}
